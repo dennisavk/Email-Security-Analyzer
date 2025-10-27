e6d691f3984710bd6473cba8acefd7eafd24998d
Dennis E. Avorkliyah <dennisavorkliyah8@gmail.com>
2025-10-27 00:52:31 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,16 @@
-# utils.py content here
+import re
+
+
+SUSPICIOUS_PHRASES = ['password', 'verify your account', 'click here', 'urgent', 'login']
+
+
+def extract_links(text):
+return re.findall(r'http[s]?://\S+', text)
+
+
+def phishing_score(text, links_count):
+score = 0
+if any(word.lower() in text.lower() for word in SUSPICIOUS_PHRASES):
+score += 5
+score += links_count
+return score
